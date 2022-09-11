c0ecb8b5ccf44a19f870e8b533d2e31cd94fb276
gasparovicsniki <gasparovics.niki@icloud.com>
2022-09-11 20:06:27 +0200

    javítás

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[N_ELEMENTS]; //N_ELEMENTS
+    std::cout << "1-100 ertekek duplazasa" << std::endl; //pontosvessző hiányzik a végéről, stringhez duplaaposztróf szükséges
+    for (int i = 0; i < N_ELEMENTS; i++) //hiányos ciklus feltétel, feltételvizsgálat hiányzik, végrehajtandó lépés minden iterációban (i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++) //i<N_ELEMENTS, hibás feltételvizsgálat
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"<< b[i]<< std::endl; // hiányzik az endline és a pontosvessző, illetve a kiírandó érték változója
     }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    std::cout << "Atlag szamitasa: " << std::endl; 
+    double atlag; //double
+    for (int i = 0; i < N_ELEMENTS; i++) //pontosvessző a sima vessző helyett
     {
-        atlag += b[i]
+        atlag += b[i]; //nincs pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
